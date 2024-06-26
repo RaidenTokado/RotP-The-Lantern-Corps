90a48dddcc60decec93ca80908bf08694325d8ad
RaidenTokado <142804862+RaidenTokado@users.noreply.github.com>
2024-06-26 19:57:13 +0500

    1.0

--- src/main/java/com/raidentokado/rotp_tlc/action/GreenLanternUnreceiving.java
+++ src/main/java/com/raidentokado/rotp_tlc/action/GreenLanternUnreceiving.java
@@ -1,10 +1,14 @@
 package com.raidentokado.rotp_tlc.action;
 
 import com.github.standobyte.jojo.action.stand.StandEntityAction;
+import com.github.standobyte.jojo.entity.stand.StandEntity;
+import com.github.standobyte.jojo.entity.stand.StandEntityTask;
+import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
 public class GreenLanternUnreceiving extends StandEntityAction {
@@ -25,6 +29,24 @@ public class GreenLanternUnreceiving extends StandEntityAction {
             }
         }
         return itemStack;
+    }
+    @Override
+    public void standPerform(@NotNull World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task){
+        if(!world.isClientSide){
+            if(power.getUser() instanceof PlayerEntity){
+                PlayerEntity player = (PlayerEntity) power.getUser();
+                for(int i=0;i<player.inventory.getContainerSize();i++){
+                    ItemStack item = player.inventory.getItem(i);
+                    Item type = item.getItem();
+                    if(!item.isEmpty()){
+                        if(type == Items.SNOWBALL){
+                            item.shrink(item.getCount());
+                        }
+
+                    }
+                }
+            }
+        }
     }
     @Override
     public boolean enabledInHudDefault() {
